16ead6adc61461b1aeee614892c61149d04fd17b
Registro Vehicular <vehicularregistro526@gmail.com>
2025-04-25 15:09:30 -0500

    Update Step3.tsx

--- src/components/InsRegistroSalida/Step3.tsx
+++ src/components/InsRegistroSalida/Step3.tsx
@@ -55,7 +55,7 @@ function StepTres({ llantasParte1, setLlantasParte1, handlePreviousStep, handleN
                     {/* Imagen de referencia - 50% del ancho - orden primero */}
                     <div className="flex justify-center items-start order-1 md:order-1">
                         <img
-                            src=".../src/assets/Inspeccion 10 llantas.png"
+                            src=".../src/assets/Inspeccion 10 llantas.jpg"
                             alt="Indicador"
                             className="max-w-full h-auto"
                         />
