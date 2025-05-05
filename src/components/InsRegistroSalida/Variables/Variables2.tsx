2ea92d496c0613963687e141d5ab337c0d53333b
Registro Vehicular <vehicularregistro526@gmail.com>
2025-05-05 10:30:44 -0500

    Update Variables2.tsx - Creamos funciones para inicializar las llantas según el tipo

--- src/components/InsRegistroSalida/Variables/Variables2.tsx
+++ src/components/InsRegistroSalida/Variables/Variables2.tsx
@@ -2,7 +2,39 @@ import { useState } from 'react';
 
 function Variables2() {
 
-  const [llantasParte1, setLlantasParte1] = useState(
+  const getInitialLlantas = (tipoVehiculo: string) => {
+    const isCamion = tipoVehiculo === 'camion';
+
+    const parte1 = [
+      { id: 1, fp: false, pe: false, pa: false, desgaste: false },
+      { id: 2, fp: false, pe: false, pa: false, desgaste: false }, 
+      { id: 5, fp: false, pe: false, pa: false, desgaste: false }, 
+      ...(isCamion ? [{ id: 6, fp: false, pe: false, pa: false, desgaste: false },]: []),];
+    
+    const parte2 = [
+      { id: 7, fp: false, pe: false, pa: false, desgaste: false },
+      ...(isCamion? [{ id: 8, fp: false, pe: false, pa: false, desgaste: false },]: []),];
+
+    return { parte1, parte2 };
+  };
+
+  const [tipoVehiculo, setTipoVehiculo] = useState('');
+  const [llantasParte1, setLlantasParte1] = useState([]);
+  const [llantasParte2, setLlantasParte2] = useState([]);
+  const [observacionGeneralLlantas, setObservacionGeneralLlantas] = useState('');
+
+  // Función para inicializar llantas cuando se selecciona el tipo
+  const initializeLlantas = (tipo: string) => {
+    const { parte1, parte2 } = getInitialLlantas(tipo);
+    setLlantasParte1(parte1);
+    setLlantasParte2(parte2);
+    setTipoVehiculo(tipo);
+  };
+
+  
+
+
+  /*const [llantasParte1, setLlantasParte1] = useState(
     Array(5).fill({ id: 0, fp: false, pe: false, pa: false, desgaste: false }).map((_, index) => ({
       id: index + 1,
       fp: false,
@@ -20,12 +52,10 @@ function Variables2() {
       pa: false,
       desgaste: false,
     }))
-  );
-
-  const [observacionGeneralLlantas, setObservacionGeneralLlantas] = useState('');
+  );*/
 
   return {
-    llantasParte1, setLlantasParte1, llantasParte2, setLlantasParte2, observacionGeneralLlantas, setObservacionGeneralLlantas
+    llantasParte1, setLlantasParte1, llantasParte2, setLlantasParte2, tipoVehiculo, setTipoVehiculo: initializeLlantas, observacionGeneralLlantas, setObservacionGeneralLlantas
   }
 };
 
