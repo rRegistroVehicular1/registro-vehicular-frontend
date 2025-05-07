041753046a7a5f77d70cf7200ef844de43785632
Registro Vehicular <vehicularregistro526@gmail.com>
2025-05-07 11:22:17 -0500

    Create llantas.d.ts

---
+++ src/types/llantas.d.ts
@@ -0,0 +1,11 @@
+export interface Llanta {
+  id: number;
+  nombre: string;
+  fp: boolean;
+  pe: boolean;
+  pa: boolean;
+  desgaste: boolean;
+}
+
+// Alias para compatibilidad
+export type Llantas = Llanta;
