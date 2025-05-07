68017643013fa4f50de2349b0daf6860f58a2368
Registro Vehicular <vehicularregistro526@gmail.com>
2025-05-07 09:26:58 -0500

    Update Step2.tsx

--- src/components/InsRegistroSalida/Step2.tsx
+++ src/components/InsRegistroSalida/Step2.tsx
@@ -1,3 +1,4 @@
+// components/InsRegistroSalida/Step2.tsx
 type Step2Props = {
     placa: string;
     setPlaca: (value: string) => void;
@@ -10,16 +11,22 @@ type Step2Props = {
     onPrevious: () => void;
     onNext: () => void;
     datos: string[];
+    actualizarLlantasPorTipo: (tipo: string) => void;
 }
 
-function StepDos({ placa, setPlaca, conductor, setConductor, tipoVehiculo, setTipoVehiculo, odometroSalida, setOdometroSalida, onPrevious, onNext, datos }: Step2Props) {
-
+function StepDos({ placa, setPlaca, conductor, setConductor, tipoVehiculo, setTipoVehiculo, odometroSalida, setOdometroSalida, onPrevious, onNext, datos, actualizarLlantasPorTipo}: Step2Props) {
+    
     const validateStep2 = () => {
         if (!placa || !conductor || !tipoVehiculo || !odometroSalida) {
             alert('Todos los campos de este paso son obligatorios.');
             return false;
         }
         return true;
+    };
+
+    const handleTipoVehiculoChange = (value: string) => {
+        setTipoVehiculo(value);
+        actualizarLlantasPorTipo(value); // Actualiza las llantas al cambiar el tipo
     };
 
 
@@ -57,7 +64,7 @@ function StepDos({ placa, setPlaca, conductor, setConductor, tipoVehiculo, setTi
                 Tipo de Vehículo:
                 <select
                     value={tipoVehiculo}
-                    onChange={(e) => setTipoVehiculo(e.target.value)}
+                    onChange={(e) => handleTipoVehiculoChange(e.target.value)}
                     className="mt-1 p-2 border rounded w-full"
                     required
                 >
