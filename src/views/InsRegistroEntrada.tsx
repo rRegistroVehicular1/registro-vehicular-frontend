82656cd3cd3da0d05b8f4071bfc05276c98fecdf
Registro Vehicular <vehicularregistro526@gmail.com>
2025-05-21 15:07:55 -0500

    Update InsRegistroEntrada.tsx

--- src/views/InsRegistroEntrada.tsx
+++ src/views/InsRegistroEntrada.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { BASE_URL } from './url';
+import { BASE_URL } from '../validation/url';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getInitialFormData, Revision } from '../components/InsRegistroEntrada/Variables/Variables1';
