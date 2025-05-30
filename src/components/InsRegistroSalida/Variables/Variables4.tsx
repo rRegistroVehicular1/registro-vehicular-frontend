9968fbd8c64ed62a0b54a17c4534a0b2908f0a07
Registro Vehicular <vehicularregistro526@gmail.com>
2025-05-30 16:15:58 -0500

    Update Variables4.tsx --> ingresando N/A en luces

--- src/components/InsRegistroSalida/Variables/Variables4.tsx
+++ src/components/InsRegistroSalida/Variables/Variables4.tsx
@@ -4,13 +4,13 @@ function Variables4(){
     
     const [luces, setLuces] = useState(
         [
-          { id: 1, nombre: 'Medias', funcionaSi: false, funcionaNo: false, },
-          { id: 2, nombre: 'Retroceso', funcionaSi: false, funcionaNo: false, },
-          { id: 3, nombre: 'Derecho', funcionaSi: false, funcionaNo: false, },
-          { id: 4, nombre: 'Izquierdo', funcionaSi: false, funcionaNo: false, },
-          { id: 5, nombre: 'Intermitentes', funcionaSi: false, funcionaNo: false, },
-          { id: 6, nombre: 'Stops', funcionaSi: false, funcionaNo: false, },
-          { id: 7, nombre: 'Cabina', funcionaSi: false, funcionaNo: false, },
+          { id: 1, nombre: 'Medias', funcionaSi: false, funcionaNo: false, funcionaNA: false, },
+          { id: 2, nombre: 'Retroceso', funcionaSi: false, funcionaNo: false, funcionaNA: false, },
+          { id: 3, nombre: 'Derecho', funcionaSi: false, funcionaNo: false, funcionaNA: false, },
+          { id: 4, nombre: 'Izquierdo', funcionaSi: false, funcionaNo: false, funcionaNA: false, },
+          { id: 5, nombre: 'Intermitentes', funcionaSi: false, funcionaNo: false, funcionaNA: false, },
+          { id: 6, nombre: 'Stops', funcionaSi: false, funcionaNo: false, funcionaNA: false, },
+          { id: 7, nombre: 'Cabina', funcionaSi: false, funcionaNo: false, funcionaNA: false, },
           { id: 8, nombre: 'Escolta', funcionaSi: false, funcionaNo: false, funcionaNA: false, },
         ]
       );
