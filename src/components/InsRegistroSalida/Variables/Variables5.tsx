d2dd974880abb13ccf45b39641a665eaf2debeb2
Registro Vehicular <vehicularregistro526@gmail.com>
2025-05-31 10:52:38 -0500

    Update Variables5.tsx --> Agregando N/A a documentacion

--- src/components/InsRegistroSalida/Variables/Variables5.tsx
+++ src/components/InsRegistroSalida/Variables/Variables5.tsx
@@ -4,14 +4,14 @@ function Variables5(){
     
     const [documentacion, setDocumentacion] = useState(
         [
-          { id: 1, nombre: 'Permiso bimensual de CBP', disponibleSi: false, disponibleNo: false },
-          { id: 2, nombre: 'Permiso anual de CBP', disponibleSi: false, disponibleNo: false },
-          { id: 3, nombre: 'Póliza de seguro', disponibleSi: false, disponibleNo: false },
-          { id: 4, nombre: 'Tarjeta de pesos y dimensiones', disponibleSi: false, disponibleNo: false },
-          { id: 5, nombre: 'Licencia de conducir tipo Comercial o H', disponibleSi: false, disponibleNo: false },
-          { id: 6, nombre: 'Hoja de Seguridad de los Productos', disponibleSi: false, disponibleNo: false },
-          { id: 7, nombre: 'Plan de Emergencia', disponibleSi: false, disponibleNo: false },
-          { id: 8, nombre: 'Registro único vehicular', disponibleSi: false, disponibleNo: false },
+          { id: 1, nombre: 'Permiso bimensual de CBP', disponibleSi: false, disponibleNo: false, disponibleNA: false },
+          { id: 2, nombre: 'Permiso anual de CBP', disponibleSi: false, disponibleNo: false, disponibleNA: false },
+          { id: 3, nombre: 'Póliza de seguro', disponibleSi: false, disponibleNo: false, disponibleNA: false },
+          { id: 4, nombre: 'Tarjeta de pesos y dimensiones', disponibleSi: false, disponibleNo: false, disponibleNA: false },
+          { id: 5, nombre: 'Licencia de conducir tipo Comercial o H', disponibleSi: false, disponibleNo: false, disponibleNA: false },
+          { id: 6, nombre: 'Hoja de Seguridad de los Productos', disponibleSi: false, disponibleNo: false, disponibleNA: false },
+          { id: 7, nombre: 'Plan de Emergencia', disponibleSi: false, disponibleNo: false, disponibleNA: false },
+          { id: 8, nombre: 'Registro único vehicular', disponibleSi: false, disponibleNo: false, disponibleNA: false },
         ]
       );
     
