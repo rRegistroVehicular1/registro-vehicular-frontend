578ed1fe1598b070a2ba25c5bf67b03e9ea3890a
Registro Vehicular <vehicularregistro526@gmail.com>
2025-05-31 09:53:46 -0500

    Update Variables4.tsx --> ingresando N/A en insumos

--- src/components/InsRegistroSalida/Variables/Variables4.tsx
+++ src/components/InsRegistroSalida/Variables/Variables4.tsx
@@ -17,14 +17,14 @@ function Variables4(){
     
       const [insumos, setInsumos] = useState(
         [
-          { id: 1, nombre: 'Primeros auxilios', disponibleSi: false, disponibleNo: false },
-          { id: 2, nombre: 'Conos', disponibleSi: false, disponibleNo: false },
-          { id: 3, nombre: 'Triángulos', disponibleSi: false, disponibleNo: false },
-          { id: 4, nombre: 'Cuñas', disponibleSi: false, disponibleNo: false },
-          { id: 5, nombre: 'Extintor', disponibleSi: false, disponibleNo: false },
-          { id: 6, nombre: 'Llanta de repuesto', disponibleSi: false, disponibleNo: false },
-          { id: 7, nombre: 'Gato', disponibleSi: false, disponibleNo: false },
-          { id: 8, nombre: 'Carretilla, según equipo', disponibleSi: false, disponibleNo: false },
+          { id: 1, nombre: 'Primeros auxilios', disponibleSi: false, disponibleNo: false, disponibleNa: false },
+          { id: 2, nombre: 'Conos', disponibleSi: false, disponibleNo: false, disponibleNa: false },
+          { id: 3, nombre: 'Triángulos', disponibleSi: false, disponibleNo: false, disponibleNa: false },
+          { id: 4, nombre: 'Cuñas', disponibleSi: false, disponibleNo: false, disponibleNa: false },
+          { id: 5, nombre: 'Extintor', disponibleSi: false, disponibleNo: false, disponibleNa: false },
+          { id: 6, nombre: 'Llanta de repuesto', disponibleSi: false, disponibleNo: false, disponibleNa: false },
+          { id: 7, nombre: 'Gato', disponibleSi: false, disponibleNo: false, disponibleNa: false },
+          { id: 8, nombre: 'Carretilla, según equipo', disponibleSi: false, disponibleNo: false, disponibleNa: false },
         ]
       );
 
