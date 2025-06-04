2af32217f415c51421cba78ae3d70ac2517848b7
Registro Vehicular <vehicularregistro526@gmail.com>
2025-06-04 11:43:51 -0500

    Update Home.tsx --> agregando enlace de fallas

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -108,6 +108,11 @@ function Home() {
           >
             {isChecking ? 'Verificando...' : 'Enviar'}
           </button>
+          <a href="/falla">
+            <button className="w-full mt-10 bg-green-500 text-white py-2 px-4 rounded">
+              Reportar una falla
+            </button>
+          </a>
         </div>
       </form>
     </div>
