f0865f9c5aa389cbd237eaa4e87cd9a8732bded4
Registro Vehicular <vehicularregistro526@gmail.com>
2025-06-08 16:56:55 -0500

    Update llantas.d.ts

--- src/types/llantas.d.ts
+++ src/types/llantas.d.ts
@@ -1,11 +1,11 @@
 export interface Llanta {
   id: number;
   nombre: string;
-  posicion: string;
+  posicion: 'delantera' | 'trasera' | 'extra-delantera' | 'extra-trasera' | 'central';
   lado: 'izquierda' | 'derecha' | 'central';
-  fp: boolean;
-  pe: boolean;
-  pa: boolean;
+  fp: boolean;    // Falta presión
+  pe: boolean;    // Presión excesiva
+  pa: boolean;    // Presión adecuada
   desgaste: boolean;
 }
 
