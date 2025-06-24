351a31c0fa4037033e2e2dfacdeff99f49f671f1
Registro Vehicular <vehicularregistro526@gmail.com>
2025-06-24 16:28:53 -0500

    Update Variables2.tsx

--- src/components/InsRegistroSalida/Variables/Variables2.tsx
+++ src/components/InsRegistroSalida/Variables/Variables2.tsx
@@ -6,14 +6,14 @@ function Variables2() {
   const todasLlantas: Llanta[] = [
     { id: 1, nombre: '1 - Delantera Izquierda', posicion: 'delantera', lado: 'izquierda', fp: false, pe: false, pa: false, desgaste: false },
     { id: 2, nombre: '2 - Delantera Derecha', posicion: 'delantera', lado: 'derecha', fp: false, pe: false, pa: false, desgaste: false },
-    { id: 3, nombre: '3 - Central Izquierda', posicion: 'central', lado: 'izquierda', fp: false, pe: false, pa: false, desgaste: false },
-    { id: 4, nombre: '4 - Central Derecha', posicion: 'central', lado: 'derecha', fp: false, pe: false, pa: false, desgaste: false },
-    { id: 5, nombre: '5 - Trasera Derecha', posicion: 'trasera', lado: 'derecha', fp: false, pe: false, pa: false, desgaste: false },
-    { id: 6, nombre: '6 - Extra Trasera Derecha', posicion: 'extra', lado: 'derecha', fp: false, pe: false, pa: false, desgaste: false },
-    { id: 7, nombre: '7 - Trasera Izquierda', posicion: 'trasera', lado: 'izquierda', fp: false, pe: false, pa: false, desgaste: false },
-    { id: 8, nombre: '8 - Extra Trasera Izquierda', posicion: 'extra', lado: 'izquierda', fp: false, pe: false, pa: false, desgaste: false },
-    { id: 9, nombre: '9 - Refuerzo Izquierda', posicion: 'refuerzo', lado: 'izquierda', fp: false, pe: false, pa: false, desgaste: false },
-    { id: 10, nombre: '10 - Refuerzo Derecha', posicion: 'refuerzo', lado: 'derecha', fp: false, pe: false, pa: false, desgaste: false }
+    { id: 3, nombre: '3 - Central Interna Derecha', posicion: 'central', lado: 'izquierda', fp: false, pe: false, pa: false, desgaste: false },
+    { id: 4, nombre: '4 - Central Externa Derecha', posicion: 'central', lado: 'derecha', fp: false, pe: false, pa: false, desgaste: false },
+    { id: 5, nombre: '5 - Trasera o Trasera Interna - Derecha', posicion: 'trasera', lado: 'derecha', fp: false, pe: false, pa: false, desgaste: false },
+    { id: 6, nombre: '6 - Trasera Externa Derecha', posicion: 'extra', lado: 'derecha', fp: false, pe: false, pa: false, desgaste: false },
+    { id: 7, nombre: '7 - Trasera o Trasera Interna - Izquierda', posicion: 'trasera', lado: 'izquierda', fp: false, pe: false, pa: false, desgaste: false },
+    { id: 8, nombre: '8 - Trasera Externa Izquierda', posicion: 'extra', lado: 'izquierda', fp: false, pe: false, pa: false, desgaste: false },
+    { id: 9, nombre: '9 - Central Interna Izquierda', posicion: 'refuerzo', lado: 'izquierda', fp: false, pe: false, pa: false, desgaste: false },
+    { id: 10, nombre: '10 - Central Externa Izquierda', posicion: 'refuerzo', lado: 'derecha', fp: false, pe: false, pa: false, desgaste: false }
   ];
 
   // Estado para las llantas seleccionadas
