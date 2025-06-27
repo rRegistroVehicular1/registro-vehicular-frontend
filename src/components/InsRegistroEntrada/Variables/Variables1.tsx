67bd6815d6746dfecfb4d49d31a9545621e6116f
Registro Vehicular <vehicularregistro526@gmail.com>
2025-06-27 16:02:13 -0500

    Update Variables1.tsx

--- src/components/InsRegistroEntrada/Variables/Variables1.tsx
+++ src/components/InsRegistroEntrada/Variables/Variables1.tsx
@@ -1,23 +1,23 @@
 export const getInitialFormData = () => {
     return {
         revisiones: [
-            { descripcion: "Revisión de golpes en unidades frente, atrás y laterales", opcion: null },
-            { descripcion: "No dejar la llave en el vehículo y cerrarlo", opcion: null },
-            { descripcion: "Dejar sin basura el camión, limpio por dentro y por fuera", opcion: null },
-            { descripcion: "Dejar apagado las luces, radio, intermitentes y rotativa", opcion: null },
-            { descripcion: "Maletín de primeros auxilios", opcion: null },
-            { descripcion: "Conos", opcion: null },
-            { descripcion: "Triángulos", opcion: null },
-            { descripcion: "Extintor", opcion: null },
-            { descripcion: "Llanta de repuesto", opcion: null },
-            { descripcion: "Gato", opcion: null },
-            { descripcion: "Carretilla, según aplique", opcion: null },
-            { descripcion: "Poner cuñas", opcion: null }
+            { descripcion: "Revisión de golpes en unidades frente, atrás y laterales", opcion: undefined },
+            { descripcion: "No dejar la llave en el vehículo y cerrarlo", opcion: undefined },
+            { descripcion: "Dejar sin basura el camión, limpio por dentro y por fuera", opcion: undefined },
+            { descripcion: "Dejar apagado las luces, radio, intermitentes y rotativa", opcion: undefined },
+            { descripcion: "Maletín de primeros auxilios", opcion: undefined },
+            { descripcion: "Conos", opcion: undefined },
+            { descripcion: "Triángulos", opcion: undefined },
+            { descripcion: "Extintor", opcion: undefined },
+            { descripcion: "Llanta de repuesto", opcion: undefined },
+            { descripcion: "Gato", opcion: undefined },
+            { descripcion: "Carretilla, según aplique", opcion: undefined },
+            { descripcion: "Poner cuñas", opcion: undefined }
         ]
     };
 };
 
 export type Revision = {
     descripcion: string;
-    opcion: boolean | null;
+    opcion: boolean | undefined;
 };
